b1989eeab836f9748d5b23386651a5b9494a6e84
PratikDeore1199 <122374744+PratikDeore1199@users.noreply.github.com>
2023-05-14 17:50:22 +0530

    added modal (dialouge)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 
+import { useState } from "react";
 import Blogs from "./components/Blogs";
+import Modal from "./components/Modal";
+
 function App() {
+  const [showCreateModal, setShowCreateModal]=useState(false)
   return (
   <div className="m-6">
     <Blogs></Blogs>
+    <Modal show={showCreateModal} onClose={()=>{setShowCreateModal(false)
+    }}><Blogs></Blogs></Modal>
+    <button  onClick={()=>{
+     setShowCreateModal(true)
+    }}>create</button>
   </div>
   );
 }
